0d49334fefc9401c3611003c87637993147bc312
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-05-12 21:06:29 +0900

    회전도 포함되게 추가

---
+++ Assets/Scripts/Environment/ObjectPause.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ObjectPause : MonoBehaviour, IPausable
+{
+    Vector2 velocityBeforePause = default;
+    float rotationBeforePause = 0f;
+
+    public void Pause()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb.linearVelocity != Vector2.zero)
+        {
+            velocityBeforePause = rb.linearVelocity;
+            rotationBeforePause = rb.angularVelocity;
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+    }
+    public void Resume()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb.linearVelocity == Vector2.zero && (velocityBeforePause != default && rotationBeforePause != 0f))
+        {
+            rb.linearVelocity = velocityBeforePause;
+            rb.angularVelocity = rotationBeforePause;
+            velocityBeforePause = default;
+            rotationBeforePause = 0f;
+        }
+    }
+}
