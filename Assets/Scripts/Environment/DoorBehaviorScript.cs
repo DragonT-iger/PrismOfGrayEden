62741159cdfd86ec722ffc9c901e781330d005be
JUNE <98323466+khjune6421@users.noreply.github.com>
2025-05-13 11:26:53 +0900

    Update DoorBehaviorScript.cs

--- Assets/Scripts/Environment/DoorBehaviorScript.cs
+++ Assets/Scripts/Environment/DoorBehaviorScript.cs
@@ -1,23 +1,35 @@
 using UnityEngine;
 
-public class DoorBehaviorScript : MonoBehaviour
+public class DoorBehaviorScript : MonoBehaviour, IPausable
 {
     [SerializeField] GameObject button;
+    bool isPaused = false;
+    public void Pause()
+    {
+        isPaused = true;
+    }
+    public void Resume()
+    {
+        isPaused = false;
+    }
 
     void Update()
     {
-        if (button.GetComponent<ButtonScript>().isButtonOn)
+        if (isPaused)
         {
-            if (transform.localPosition.y <= 1.0f)
+            if (button.GetComponent<ButtonScript>().isButtonOn)
             {
-                transform.localPosition += new Vector3(0.0f, 0.5f, 0.0f) * Time.deltaTime;
+                if (transform.localPosition.y <= 1.0f)
+                {
+                    transform.localPosition += new Vector3(0.0f, 0.5f, 0.0f) * Time.deltaTime;
+                }
             }
-        }
-        else
-        {
-            if (transform.localPosition.y >= 0.0f)
+            else
             {
-                transform.localPosition += new Vector3(0.0f, -0.5f, 0.0f) * Time.deltaTime;
+                if (transform.localPosition.y >= 0.0f)
+                {
+                    transform.localPosition += new Vector3(0.0f, -0.5f, 0.0f) * Time.deltaTime;
+                }
             }
         }
     }
